905a56ca005cbc55060ad291e5cfa4dc1ff93ba2
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-03-29 10:09:49 +0430

    Add Login view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserChange, CustomUserCreation
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreation
+    form = CustomUserChange
+    model = CustomUser
+    list_display = ['username', 'email', 'age', 'is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),)
+    add_fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('age',)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ accounts/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+
+class CustomUserCreation(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('age', )
+
+
+class CustomUserChange(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
