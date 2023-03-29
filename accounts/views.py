20f2648a4bb6a9e764211348073e3e550747dd9f
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-03-29 11:24:04 +0430

    Complete Signup view

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
 
-from .forms import UserCreationForm
+from .forms import CustomUserCreation
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreation
     template_name = 'registration/signup.html'
     success_url = reverse_lazy('login')
