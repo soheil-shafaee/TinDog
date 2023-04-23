aed5bf12af132205cf06387ea891ba656a53fff5
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-04-23 06:26:24 +0430

    Add Signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view, name='signup'),
-    #     path('login/', views.LogInView.as_view, name='login')
+    path('signup/', views.SignUpView.as_view(), name='signup'),
+    #     path('login/', views.LogInView.as_view(), name='login')
 ]
