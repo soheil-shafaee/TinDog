d284c9ac5d157e21d7d057e4b66b460ba71df065
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2023-07-01 10:24:28 +0430

    Added static files for css and fonts

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -146,3 +145,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Media files:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
+# Static files config:
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
